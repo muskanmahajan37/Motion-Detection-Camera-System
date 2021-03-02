b9ca3818b3f8da0b8c69359c2d8eb9bd304ed8f0
Cyebukayire <peacecyebukayire@gmail.com>
2021-03-02 15:12:44 +0200

    initial commit

--- basic-motion-detection.py
+++ basic-motion-detection.py
@@ -1,14 +1,11 @@
 import cv2
-import numpy as numpy
-cap = cv2.videoCapture(0)
+cap = cv2.VideoCapture(0) # Capturing the video via default camera
 
-while cap.isOpened() :
+while cap.isOpened() : # Loop to display every single frame from the captured video
     ret, frame = cap.read()
-
-    cv2.imshow("inter", frame)
-
-    if cv2.waitKey(40) == 27:
+    cv2.imshow("Motion Detection Camera System", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'): #The program quits when the user presses "q"
         break
 
-cv2.destroyAllWindows()
-cap.release()
+cv2.destroyAllWindows() # CLoses all the opened windows
+cap.release() # Releases the video
