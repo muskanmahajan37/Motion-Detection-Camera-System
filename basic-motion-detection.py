18baadb0c5e19c0ff04ae6bbbcc11fbaad7d261c
Cyebukayire <peacecyebukayire@gmail.com>
2021-03-13 18:19:39 +0200

    Sending Email to user

--- basic-motion-detection.py
+++ basic-motion-detection.py
@@ -1,10 +1,42 @@
 import cv2
-from playsound import playsound
+import playsound
+import threading
+lock = threading.Lock() #This will help us perform synchronization; ex:to stop other threads from sending more emails
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
 cap = cv2.VideoCapture(0) # Capturing the video via default camera
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
-alarmStatus=0;
+alarm_triggered = False
+email_sent = False
 
+def send_mail_function():
+    senderEmail = "robeehacks@gmail.com"
+    senderPassword = "1pa2345w"
+    recipientEmail = "peacecyebukayire@gmail.com"
+    recipientEmail = recipientEmail.lower()
+    Email_subject = "Intruder Has Been Detected!"
+    Email_body = "Warning! An intruder has been reported in ABC Room"
+    message = 'Subject: {}\n\n{}'.format(Email_subject, Email_body)
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(senderEmail, senderPassword)
+        server.sendmail(senderEmail, recipientEmail, message)
+        print("sent to {}".format(recipientEmail))
+        server.close()
+    except Exception as e:
+    	print(e)
+
+# threading.Thread(target=send_mail_function).start()
+
+def play_alarm_sound_function():
+    	while True:
+		playsound.playsound('Alarm Sound.mp3',True)
 
 while cap.isOpened() : # Loop to draw rectangles on large moving object
     diff = cv2.absdiff(frame1,frame2)
@@ -17,16 +49,18 @@ while cap.isOpened() : # Loop to draw rectangles on large moving object
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
         
-        if cv2.contourArea(contour) < 4000:
-            continue
-        cv2.rectangle(frame1, (x,y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(frame1, "Status: {}".format("Intruder"), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-        if(alarmStatus == 0):
-            playsound('alarm.mp3')
-            alarmStatus = 1
-            break
+        if cv2.contourArea(contour) > 4000:
+            cv2.rectangle(frame1, (x,y), (x+w, y+h), (0, 255, 0), 2)
+            cv2.putText(frame1, "Status: {}".format("Intruder"), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+            with lock:
+                if not email_sent:
+                    threading.Thread(target=send_mail_function).start()
+                    email_sent = True
+                # if not alarm_triggered:
+                #     threading.Thread(target = play_alarm_sound_function).start()
+                #     alarm_triggered = True
 
-    # cv2.drawContours(frame1, contours, -1, (0,255,0), 2) 
+    cv2.drawContours(frame1, contours, -1, (0,255,0), 2) 
     cv2.imshow("Motion Detection Camera System", frame1)
     frame1 = frame2
     ret, frame2 = cap.read()
