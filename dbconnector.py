c9ec148f94486c9d73928ee2bdc73605f184780d
Cyebukayire <peacecyebukayire@gmail.com>
2021-05-24 12:07:07 +0200

    Notification module

--- dbconnector.py
+++ dbconnector.py
@@ -1,12 +1,6 @@
 from configparser import ConfigParser
 
-
 def read_db_config(filename='config.ini', section='mysql'):
-    """ Read database configuration file and return a dictionary object
-    :param filename: name of the configuration file
-    :param section: section of database configuration
-    :return: a dictionary of database parameters
-    """
     # create parser and read ini configuration file
     parser = ConfigParser()
     parser.read(filename)
